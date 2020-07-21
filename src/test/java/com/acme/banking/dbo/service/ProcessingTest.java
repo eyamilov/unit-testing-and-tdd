6347d5456ce5618087dfba6608aee3f1bf18c8ae
YAMILOV Evgeny <Evgeny.YAMILOV@raiffeisen.ru>
2020-07-22 00:44:17 +0300

    tmp

---
+++ src/test/java/com/acme/banking/dbo/service/ProcessingTest.java
@@ -0,0 +1,16 @@
+package com.acme.banking.dbo.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProcessingTest {
+//    @Test
+//    public void shouldCreateClientWhenCorrectParams() {
+//        Processing sut = new Processing();
+//        String name = mock
+//        Assert.assertNull(sut.createClient("TestName"));
+//    }
+//
+//    @Test
+//    public void should
+}
